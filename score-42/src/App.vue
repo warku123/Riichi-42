f13099da038757d3f2e27d49901aa94d40e57359
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-11-23 10:38:23 +0800

    init

---
+++ score-42/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h2>数据输入</h2>
+    <form @submit.prevent="submitData">
+      <label for="dataInput">输入内容：</label>
+      <input type="text" v-model="inputData">
+      <input type="submit" value="提交">
+    </form>
+
+    <h2>数据展示</h2>
+    <table>
+      <tr>
+        <th>内容</th>
+      </tr>
+      <tr v-for="(item, index) in dataItems" :key="index">
+        <td>{{ item }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      inputData: '',
+      dataItems: []
+    }
+  },
+  methods: {
+    submitData() {
+      if (this.inputData.trim() === '') return;
+      this.dataItems.push(this.inputData);
+      this.inputData = '';
+    }
+  }
+}
+</script>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;
+}
+</style>
